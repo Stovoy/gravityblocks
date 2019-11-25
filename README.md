55c64a9f5d3c47e7c9d545a82afa70913420a3a8
Steve Mostovoy <stevemostovoysm@gmail.com>
2019-11-25 00:49:37 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gravity Blocks mod for Minecraft
+
+This is work in progress, not yet functional!
+
+-- Bugs
+When block breaks, make particles fit the texture
+When block breaks, use loot table properly to drop itself
+
+-- TODO
+Make block harder to break
+Give block a recipe
+On tick, store vector for each entity. update vectors based on distance to each block, then move entities
+Figure out gravity block destruction
+Interactions with water flow
---
+++ src/main/java/com/stovoy/gravityblocks/GravityBlocks.java
@@ -0,0 +1,86 @@
+package com.stovoy.gravityblocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.ObjectHolder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.function.Supplier;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(GravityBlocks.MODID)
+public class GravityBlocks {
+    static final String MODID = "gravityblocks";
+
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    private static final ItemGroup MOD_ITEM_GROUP = new ModItemGroup(MODID,
+            () -> new ItemStack(ModItems.GRAVITY_BLOCK_ITEM));
+
+    public GravityBlocks() {
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MODID)
+    public static class ModEventSubscriber {
+        @SubscribeEvent
+        public static void onRegisterBlocks(final RegistryEvent.Register<Block> event) {
+            event.getRegistry().registerAll(
+                    setup(new Block(Block.Properties.create(
+                            new Material.Builder(MaterialColor.STONE).build()
+                    ).hardnessAndResistance(1.0F, 1.0F)), "gravity_block")
+            );
+        }
+
+        @SubscribeEvent
+        public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(
+                    setup(new BlockItem(ModItems.GRAVITY_BLOCK, new Item.Properties().group(MOD_ITEM_GROUP)), "gravity_block_item")
+            );
+        }
+
+        static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+            return setup(entry, new ResourceLocation(MODID, name));
+        }
+
+        static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+            entry.setRegistryName(registryName);
+            return entry;
+        }
+    }
+
+    public static class ModItemGroup extends ItemGroup {
+        private final Supplier<ItemStack> iconSupplier;
+
+        ModItemGroup(final String name, final Supplier<ItemStack> iconSupplier) {
+            super(name);
+            this.iconSupplier = iconSupplier;
+        }
+
+        @Override
+        public ItemStack createIcon() {
+            return iconSupplier.get();
+        }
+    }
+
+    @ObjectHolder(MODID)
+    public static class ModItems {
+        public static final BlockItem GRAVITY_BLOCK_ITEM = null;
+        public static final Block GRAVITY_BLOCK = null;
+    }
+}
